b7613868fd7fc4437d29d8bdb9a2272c5b7e50d7
Eduardo Alcantara de Oliveira <eduardo_alcantara@hotmail.com>
2018-07-29 22:29:59 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+//Este decorator que transforma o pacote em um componente
+// 29/07/18
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+  title = 'Angular 6';
+  description='Leao';
+  url= 'http://sinpma.com.br/wp-content/uploads/2017/04/link.jpg';
 }
