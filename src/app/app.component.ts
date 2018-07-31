52ee72e7d6ba9e9a426f8f1cba4d39ec1b4e80e7
Eduardo Alcantara de Oliveira <eduardo_alcantara@hotmail.com>
2018-07-31 07:53:29 -0300

    ngmodel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular 6';
-  description='Leao';
-  url= 'http://sinpma.com.br/wp-content/uploads/2017/04/link.jpg';
+
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    }
+  ];
+
 }
